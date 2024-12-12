4710565ef6d16531a04db24786463bfc6e9148f6
minpooh <minjoung1510@gmail.com>
2024-12-12 22:29:50 +0900

    start

---
+++ src/main/java/com/minpooh/factory/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.minpooh.factory.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping(value = "")
+    public String main() {
+        return "main";
+    }
+}
