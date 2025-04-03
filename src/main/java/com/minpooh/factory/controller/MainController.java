23148000193a8671c68bc7d85fa6a91696e45135
minpooh <minjoung1510@gmail.com>
2025-04-03 22:18:23 +0900

    change name main >> index

--- src/main/java/com/minpooh/factory/controller/MainController.java
+++ src/main/java/com/minpooh/factory/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping(value = "")
     public String main() {
-        return "main";
+        return "index";
     }
 
     @GetMapping(value = "/about")
